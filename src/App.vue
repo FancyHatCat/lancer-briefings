530434bf0762d15730f5899e285360b6789a9228
FancyHatCat <67059397+FancyHatCat@users.noreply.github.com>
2024-02-09 12:14:00 -0500

    JJJJ

--- src/App.vue
+++ src/App.vue
@@ -110,36 +110,43 @@ export default {
           "callsign": "Azure",
           "alias": "Roy Spencer",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
+          "status": "ACTIVE"
         },
         {
           "callsign": "Jackal",
           "alias": "Layla Nour",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
+          "status": "ACTIVE"
         },
         {
           "callsign": "Lazarus",
           "alias": "Helios Nonagessimus",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          "status": "ACTIVE"
         },
         {
           "callsign": "Lysis",
           "alias": 'Erien Vachs',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "status": "ACTIVE"
         },
         {
           "callsign": "Mayhem",
           "alias": 'Bex',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "status": "ACTIVE"
         },
         {
           "callsign": "Mercury",
           "alias": 'Kaelyn Forsythe',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "status": "ACTIVE"
         },
         {
           "callsign": "Sequence",
           "alias": 'Qasem Nurani',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "status": "ACTIVE"
         },
       ],
       "header": {
